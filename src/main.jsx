e0a75ec80e39397f99af21f99a35ba6c8da9c93f
Maulana2025-pal <lmx010804@gmail.com>
2025-06-05 14:20:55 +0700

    dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import {BrowserRouter, Route, Routes} from 'react-router'
-import HomePage from './pages/HomePage'
-import KontakSection from './Components/Kontak-Section'
-import AboutPages from './pages/AboutPages'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import KontakSection from "./Components/Kontak-Section";
+import AboutPages from "./pages/AboutPages";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-  <BrowserRouter>
-  <Routes>
-    <Route path='/' element={<HomePage />} />
-    <Route path='/kontak' element={<KontakSection/>}/>
-    <Route path='/about' element={<AboutPages/>}/>
-  </Routes>
-  </BrowserRouter>
-
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/kontak" element={<KontakSection />} />
+        <Route path="/about" element={<AboutPages />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
