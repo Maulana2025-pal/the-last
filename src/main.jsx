8f9130cbc6798d1a40d4d867060dcfd0cac56a13
Maulana2025-pal <lmx010804@gmail.com>
2025-06-09 16:37:09 +0700

    main

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage";
 import KontakSection from "./Components/Kontak-Section";
 import AboutPages from "./pages/AboutPages";
 import Shop from "./pages/Shop";
+import ShopPage from "./Components/ShopPage";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -14,7 +15,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/" element={<HomePage />} />
         <Route path="/kontak" element={<KontakSection />} />
         <Route path="/about" element={<AboutPages />} />
-        <Route path="/shop" element={<Shop />} />
+        <Route path="/shop" element={<ShopPage />} />
         <Route path="/cart" element={<CartPage />} />
       </Routes>
     </BrowserRouter>
